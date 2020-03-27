84e92386e8260d10069c93716495455cc8909b64
Ulieana <62381290+yuliana15@users.noreply.github.com>
2020-03-28 00:17:27 +0700

    minngu4/control-flow

---
+++ latihan.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+ <html lang= "en">
+ <head>
+
+     <meta charset= "UTF-8">
+
+     <title>
+     	latihan loopping tabel
+     	</title>
+     		</head>
+     			<body>
+
+<h2>
+	Membuat perulangan tabel
+	  </h2>
+	  <table berder="1" cellpadding="20" cellspacing="0">
+	  	for ($b = 1; $b <= 20; $b++){
+	  	if ($b % 2 === 1 ){
+	  		echo "<tr bgcolor= '#ff9990>";
+	  	}
+	  	else 
+	  		echo "<tr>";
+
+
+	  		for ($k=1 <= 8; $k++){
+	  			echo "<td> $b,$k </td>";
+	  		}
+
+	  		echo "</tr>";
+
+	  	}   			
+	  	?>
+	  	</table>
+	  </body>
+	</html>
